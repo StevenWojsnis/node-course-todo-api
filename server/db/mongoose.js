342b0adb834ab4a2c0c6aba87e65ba94132d70cd
Steve Wojsnis <stevewojsnis@Steves-MBP.fios-router.home>
2017-07-25 14:44:43 -0400

    Attempted fix for heroku app 3.0

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true});
-mongoose.connect(`mongodb://${process.env.dbuser}:${process.env.dbpassword}@ds121483.mlab.com:21483/sandbox_practice`, {useMongoClient: true});
-
+//mongoose.connect(`mongodb://${process.env.dbuser}:${process.env.dbpassword}@ds121483.mlab.com:21483/sandbox_practice`, {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 module.exports = {
   mongoose
 };
